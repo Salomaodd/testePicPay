0c7a40057e47e760a2d32d6b6dbbe6392f37db68
Salomao Dias <salomaodiasdiniz22@gmail.com>
2023-11-12 21:07:22 -0300

    Created Transaction controller

--- src/main/java/com/picpay/controllers/UserController.java
+++ src/main/java/com/picpay/controllers/UserController.java
@@ -6,20 +6,19 @@ import com.picpay.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RestController("/users")
+@RestController
+@RequestMapping("/users")
 public class UserController {
 
     @Autowired
     private UserService userService;
 
     @PostMapping
-    public ResponseEntity<User> createUser(UserDTO userDTO){
+    public ResponseEntity<User> createUser(@RequestBody UserDTO userDTO){
         User newUser = userService.createUser(userDTO);
         return new ResponseEntity<>(newUser, HttpStatus.CREATED);
     }
--- src/main/java/com/picpay/services/TransactionService.java
+++ src/main/java/com/picpay/services/TransactionService.java
@@ -26,7 +26,10 @@ public class TransactionService {
     @Autowired
     private RestTemplate restTemplate;
 
-    public void createTransaction(TransactionDTO transaction) throws Exception {
+    @Autowired
+    private NotificationService notificationService;
+
+    public Transaction createTransaction(TransactionDTO transaction) throws Exception {
 
         User sender = this.userService.findUserById(transaction.senderId());
         User receiver = this.userService.findUserById(transaction.receiverId());
@@ -35,7 +38,7 @@ public class TransactionService {
 
         boolean isAuthorized = this.authorizeTransaction(sender, transaction.value());
         if (!isAuthorized) {
-            throw new Exception("Tranzação não autorizada");
+            throw new Exception("Transação não autorizada");
         }
 
         Transaction newTransaction = new Transaction();
@@ -50,6 +53,11 @@ public class TransactionService {
         this.transactionRepository.save(newTransaction);
         this.userService.saveUser(sender);
         this.userService.saveUser(receiver);
+
+        this.notificationService.sendNotification(sender, "Transação realizada com sucesso");
+        this.notificationService.sendNotification(receiver, "Transação recebida com sucesso");
+
+        return newTransaction;
 
         }
 
